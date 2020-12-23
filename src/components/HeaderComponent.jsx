5f667a01a45b11d4b793b137fd1b1473db995b0a
justme <just-k@gmx.net>
2020-12-23 01:19:47 +0100

    gcp

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -14,7 +14,8 @@ class HeaderComponent extends Component {
             <div>
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                        <div> <a href="http://localhost:3000/" className="navbar-brand">Employee Management App</a></div>
+                        <div> <a href="/" className="navbar-brand">Employee Management App</a></div>
+                        <div> { window.location.href } </div>
                     </nav>
                 </header>
             </div>
