3e5dd603ca7132c27cd6de8ec4e81745b3ed1d27
justme <just-k@gmx.net>
2020-12-23 01:06:57 +0100

    gcp

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "https://sylvan-deck-298611.ey.r.appspot.com/api/v1/employees";
 
 class EmployeeService {
     getEmplyees(){
